4bdc496a87daefd7b333a342a337039c0394901a
Anatolii <zavorinanatolii@mail.ru>
2022-08-06 10:12:25 +0800

    Задание 1

---
+++ dz-1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 1 число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a} , min = {b}");
+}
+else
+{
+    Console.WriteLine($"max = {b} , min = {a}");
+}
