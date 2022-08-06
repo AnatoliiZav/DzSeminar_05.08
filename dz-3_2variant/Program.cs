5ca216b3cadc5f78a61521666db001ef1b8ef61e
Anatolii <zavorinanatolii@mail.ru>
2022-08-06 17:06:57 +0800

    Дз 2 вариант

---
+++ dz-3_2variant/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"{a} является четным");
+}
+else
+{
+    Console.WriteLine($"{a} является нечетным");
+}
