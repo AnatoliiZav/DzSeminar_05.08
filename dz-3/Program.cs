b4d7d67f0e3e85289431c4a5d123abccbba198af
Anatolii <zavorinanatolii@mail.ru>
2022-08-06 13:04:14 +0800

    Сделал дз 3

--- dz-3/Program.cs
+++ dz-3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+int b = a / 2;
+
+if (b * 2 == a)
+{
+    Console.WriteLine($"{a} является четным");
+}
+else
+{
+    Console.WriteLine($"{a} является нечетным");
+}
