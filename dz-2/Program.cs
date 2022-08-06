9e0207a2ac1697324070077da1130fd5a75cfb71
Anatolii <zavorinanatolii@mail.ru>
2022-08-06 10:34:07 +0800

    Задание 2

---
+++ dz-2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1 число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите 3 число: ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+{
+    Console.WriteLine($"max = {max}");
+}
