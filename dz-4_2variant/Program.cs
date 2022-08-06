f6dfb6091e3176693cb15f7491c2eb39967cba00
Anatolii <zavorinanatolii@mail.ru>
2022-08-06 18:13:32 +0800

    Добавил 2вариант 4 задания

---
+++ dz-4_2variant/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число больше 1: ");
+int N = int.Parse(Console.ReadLine());
+
+int a = 1;
+while (a <= N)
+{
+    if (a % 2 == 0)
+    {
+        Console.Write($"{a} ");
+        a = a + 1;
+    }
+    else
+    {
+       a = a + 1;
+    }
+    
+}
